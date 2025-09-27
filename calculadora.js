197bfa162fb37a7ea3faa5f87ef87d643feaba1b
jvbonat <jvab99@gmail.com>
2025-09-26 23:20:10 -0300

    Changes in branch dev

--- calculadora.js
+++ calculadora.js
@@ -21,7 +21,18 @@ function potencia(a, b) {
   return a ** b;
 }
 
+function raizQuadrada(a) {
+  if (a < 0) {
+    return "Erro: número negativo!";
+  }
+  return Math.sqrt(a);
+}
+
+console.log("√16 =", raizQuadrada(16));
+
 console.log("2 + 3 =", somar(2, 3));
+console.log("√16 =", raizQuadrada(16));
 console.log("7 - 4 =", subtrair(7, 4));
 console.log("3 * 5 =", multiplicar(3, 5));
 console.log("10 / 2 =", dividir(10, 2));
+console.log("2 ^ 4 =", potencia(2, 4));
