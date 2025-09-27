1a1d9aa1625c72c6c40d952cb8ca81f02b579ff0
jvbonat <jvab99@gmail.com>
2025-09-26 23:10:49 -0300

    Calculadora - Incremento de Funcionalidades

--- calculadora.js
+++ calculadora.js
@@ -6,5 +6,18 @@ function subtrair(a, b) {
   return a - b;
 }
 
+function multiplicar(a, b) {
+  return a * b;
+}
+
+function dividir(a, b) {
+  if (b === 0) {
+    return "Erro: divisão por zero!";
+  }
+  return a / b;
+}
+
 console.log("2 + 3 =", somar(2, 3));
 console.log("7 - 4 =", subtrair(7, 4));
+console.log("3 * 5 =", multiplicar(3, 5));
+console.log("10 / 2 =", dividir(10, 2));
