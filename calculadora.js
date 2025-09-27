a4e0ab03394436a46fe32ec5a12816b8502c6456
jvbonat <jvab99@gmail.com>
2025-09-26 23:12:23 -0300

    Calculadora - Incremento de funcionalidades 2

--- calculadora.js
+++ calculadora.js
@@ -17,6 +17,10 @@ function dividir(a, b) {
   return a / b;
 }
 
+function potencia(a, b) {
+  return a ** b;
+}
+
 console.log("2 + 3 =", somar(2, 3));
 console.log("7 - 4 =", subtrair(7, 4));
 console.log("3 * 5 =", multiplicar(3, 5));
